81f043fcda903fe2b348c50b0e450a8e48eb9cea
DESKTOP-DOGVPUB\SSAFY <ryuhaun0321@gmail.com>
2023-03-02 17:42:10 +0900

    0302

--- defi-staking-app-starter-main/defi-staking-app-starter/src/contracts/Tether.sol
+++ defi-staking-app-starter-main/defi-staking-app-starter/src/contracts/Tether.sol
@@ -5,4 +5,56 @@ contract Tether {
   string public symbol = 'USDT';
   uint256 public totalSupply = 1000000000000000000000000;  // 토큰 100만개
   uint8 public decimals = 18;
+
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint _value
+  );
+
+  // owner가 승인하고 spender에게 보냄
+  event Approval(
+    address indexed _owner,
+    address indexed _spender,
+    uint _value
+  );
+
+  mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
+  
+  constructor() public {
+    balanceOf[msg.sender] = totalSupply;
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    require(balanceOf[msg.sender] >= _value);
+    
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+
+    emit Transfer(msg.sender, _to, _value);
+
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+    allowance[msg.sender][_spender] = _value;
+    
+    emit Approval(msg.sender, _spender, _value);
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    require(balanceOf[_from] >= _value);
+    require(allowance[_from][msg.sender] >= _value);
+    
+    balanceOf[_from] -= _value;
+    balanceOf[_to] += _value;
+
+    allowance[msg.sender][_from] -= _value;
+
+
+    emit Transfer(_from, _to, _value);
+  }
+
+
 }
