61c08d278845d734effd8d4144e8f41318045733
DESKTOP-DOGVPUB\SSAFY <ryuhaun0321@gmail.com>
2023-03-07 15:38:15 +0900

    0307

--- defi-staking-app-starter-main/defi-staking-app-starter/migrations/2_deploy_contracts.js
+++ defi-staking-app-starter-main/defi-staking-app-starter/migrations/2_deploy_contracts.js
@@ -2,13 +2,23 @@ const Tether = artifacts.require('Tether')
 const RWD = artifacts.require('RWD')
 const DecentralBank = artifacts.require('DecentralBank')
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network, accounts) {
   // Deploy Mock Tether Contract
   await deployer.deploy(Tether)
+  const tether = await Tether.deployed()
 
   // Deploy RWD Contract
   await deployer.deploy(RWD)
+  const rwd = await RWD.deployed()
 
   // Deploy RWD Contract
-  await deployer.deploy(DecentralBank)
+  await deployer.deploy(DecentralBank, rwd.address, tether.address)
+  const decentralBank = await DecentralBank.deployed()
+
+
+  // 모든 리워드 토큰을 decentral bank로 전송
+  await rwd.transfer(decentralBank.address, '1000000000000000000000000')
+
+  // 투자자에게 100 Tether 전송
+  await tether.transfer(accounts[1], '1000000000000000000')
 };
